6ddc0721e007c28a071d788fb7b42432889a6c07
siddick <mebramsha@paypal.com>
2013-09-02 12:27:59 +0530

    Deploy a436ade833ddeabb7bc98eed0711ba1b9edade38  (a436ade )

--- js/sdk.js
+++ js/sdk.js
@@ -9,11 +9,36 @@ jQuery(function($){
   $('body').scrollspy({ target: ".sidebar", offset: 200 });
   $('.sidebar').affix({});
 
+  // Animation for service menus in sidebar
   $('#sdk-menus li').click(function(){
+    var link = $(this).children('a').attr('href');
     $('html, body').animate({
-        scrollTop: $($(this).children('a').attr('href')).offset().top
+        scrollTop: $(link).offset().top
     }, 400);
+
+    _gaq.push(['_trackEvent', "Click Service", link]);
     return false
   })
 
+  // Track tab click event using google analytics
+  $('a[data-toggle="tab"]').on('shown', function (e) {
+    var link = $(e.target).attr('href');
+    _gaq.push(['_trackEvent', "Click Tab", link]);
+  });
+
+  function trackOutboundLink(link) {
+    try {
+      _gaq.push(['_trackEvent', "Outbound Links", link.href]);
+      setTimeout(function() {
+        document.location.href = link.href;
+      }, 100);
+      return false;
+    } catch(err){
+      return true;
+    }
+  }
+
+  jQuery(document).on('click', 'a[href^="http"]', function(){
+    return trackOutboundLink(this);
+  })
 });
